083ed888b7a47a5d9be9fca5cebc213ebb5aeef9
yash avhad <avhad.yash@esds.co.in>
2024-08-05 19:03:24 +0530

    added must use plugins

---
+++ wp-content/mu-plugins/vipassana-post-type.php
@@ -0,0 +1,16 @@
+<?php 
+
+function custom_post_types() {
+	register_post_type('event', array(
+		'public' => true,
+		'labels' => array(
+			'name' => 'Events',
+			'add_new_item' => 'Add New Event',
+			'edit_item' => 'Edit Event',
+			'all_items' => 'All Events',
+			'singular_name' => 'Event' 
+		),
+		'menu_icon' => 'dashicons-calendar-alt'
+	));
+}
+add_action('init', 'custom_post_types');
--- wp-content/themes/vipassana/functions.php
+++ wp-content/themes/vipassana/functions.php
@@ -14,8 +14,8 @@ function custom_features() {
 	
 	add_theme_support('title-tag');
 }
-
-
 add_action('after_setup_theme', 'custom_features');
+
+
 
 ?>
