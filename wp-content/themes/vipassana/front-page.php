b9f444dda336d3308a5715e335b75c7181ae354b
yash avhad <avhad.yash@esds.co.in>
2024-08-06 19:08:40 +0530

    custome field enabled for date

--- wp-content/themes/vipassana/front-page.php
+++ wp-content/themes/vipassana/front-page.php
@@ -29,7 +29,7 @@
             $eventDate = new DateTime(get_field('add_event_date'));
             echo $eventDate->format('M');
           ?></span>
-            <span class="event-summary__day"><?php the_time('d') ?></span>
+            <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
